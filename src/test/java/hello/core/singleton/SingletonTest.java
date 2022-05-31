a3858abf9ecef58bbe79469a4bd79b82e835ecd3
myAcc <85468010+todaysystems@users.noreply.github.com>
2022-05-31 14:52:47 +0900

    220531 singleton에서 주의해야할점 무상태로 유지해야한다

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -5,6 +5,8 @@ import hello.core.member.MemberService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -41,5 +43,29 @@ public class SingletonTest {
         //isSameAs java에서의 == 객체비교할때쓰는거
         //isEqualTo java에서의 equals 실제값 비교
 
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer(){
+
+        //기존방식을 스프링 컨테이너를 사용하는 방법으로 변경
+//        AppConfig appConfig = new AppConfig();
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        //1. 조회 : 호출할 때 마다 객체를 생성
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+
+        //2. 조회 : 호출할 때 마다 객체를 생성
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        //참조값이 다른 것을 확인
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        //조회할때 마다 spring 이 처음 컨테이너에서 bean 등록한거를 계속 반환해준다 (결국 같은거다)
+        //memberService에 들어가보면 기존에 작성했던 singleton 관련 코드가 하나도없지만 spring bean이 singleton으로 관리하는것이다
+        //memberService1 == memberService2
+        assertThat(memberService1).isSameAs(memberService2);
     }
 }
